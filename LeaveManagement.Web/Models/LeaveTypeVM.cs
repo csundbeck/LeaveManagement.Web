f7aa256d1db45c678f4138c3bf6d2e2057bcd45a
Connor Sundbeck <csundbeck04@gmail.com>
2022-02-25 15:42:41 -0600

    Leave Type Management Module interaction

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+
+        public int Id { get; set; }
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Number of days should be between 1 and 25.")]
+        public int DefaultDays { get; set; }
+    }
+}
